86b2b2f7bf3e85fb56fac0ce9823a8a53e95efeb
Debatra Chatterjee <debatra.chatterjee.24@gmail.com>
2022-03-09 14:31:27 +0530

    Queue implementation completed

--- Data Structures/queue.cpp
+++ Data Structures/queue.cpp
@@ -52,13 +52,85 @@ void ArrayDisplay(){  //Not a real queue function
     cout<<'\n';
 }
 
+// Linked List Based Implementation
+// Logic - We have two pointers front and rear initially pointing to null. When we push for first time, we make front point to head after pushing. Every time we push, we make rear point to the last element of the list. Now, we can both push and pop in O(1) time.
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* LLQueue = nullptr;
+Node* LLfront = nullptr;
+Node* LLrear = nullptr;
+
+bool LLIsEmpty(){
+    return LLQueue == nullptr;
+}
+
+void LLPush(int x){
+    Node* temp = new Node();
+    temp->data = x;
+    temp->next = nullptr;
+    if(LLIsEmpty()){
+        LLQueue = temp;
+        LLfront = LLQueue;
+        LLrear = LLQueue;
+    }
+    else {
+        LLrear -> next = temp;
+        LLrear = temp;
+    }
+}
+
+void LLPop(){
+    if(LLIsEmpty()){
+        cout<<"Queue is empty"<<'\n';
+        return;
+    }
+    Node* temp = LLfront;
+    LLQueue = temp->next;
+    LLfront = temp->next;
+    delete temp;
+}
+
+int LLPeek(){
+    return LLfront->data;
+}
+
+void LLDisplay(){
+    Node* temp = LLQueue;
+    while(temp != nullptr){
+        cout<<temp->data<<' ';
+        temp = temp->next;
+    }
+    cout<<'\n';
+}
+
 int main(){
-    ArrayPop();
-    ArrayPush(3);
-    ArrayPush(4);
-    ArrayPush(5);
-    ArrayPeek();
-    ArrayPop();
-    ArrayPeek();
-    ArrayDisplay();
+    // ArrayPop();
+    // ArrayPush(3);
+    // ArrayPush(4);
+    // ArrayPush(5);
+    // ArrayPeek();
+    // ArrayPop();
+    // ArrayPeek();
+    // ArrayDisplay();
+
+    LLPush(3);
+    LLPop();
+    LLPush(4);
+    LLPush(5);
+    LLPush(6);
+    LLPush(7);
+    LLPush(8);
+    LLPush(9);
+    cout<<LLPeek()<<'\n';
+    LLPop();
+    cout<<LLPeek()<<'\n';
+    LLPop();
+    LLPop();
+    cout<<LLPeek()<<'\n';
+    LLPop();
+    LLDisplay();
 }
